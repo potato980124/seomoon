80f5ca696bc8896ed6711132e71581d2e7b3ca8e
potato990124 <potato980124@gmail.com>
2022-11-28 23:00:14 +0900

    메인비쥬얼 바닐라 자바스크립트 구현 finish

--- public/js/mainvisual.js
+++ public/js/mainvisual.js
@@ -1,59 +1,30 @@
-let mv = document.querySelector('.main_visual');
-let mvSlides = document.querySelector('.main_visual_slides');
-let mvBtn = document.querySelectorAll('.mv_btn');
-let mv_slide = document.querySelectorAll('.mv_slide');
-
-
-
-let mvSlidesWidth = mvSlides.clientWidth;
-console.log(mvSlidesWidth);
-mvBtn.forEach((e)=>{
-    e.addEventListener('click',(e)=>{
-        mv_slide.forEach((e)=>{
-            e.setAttribute('style','opacity:0');
-        })
-        mvBtn.forEach((e)=>{
-            e.classList.remove('mv_btn_active');    
-        })
-        if(e.target == mvBtn[0]){
-            let mvSlideNum = 0;
-            let left = mvSlidesWidth*mvSlideNum;
-            mvSlides.setAttribute('style',`left:${-left}px`);
-            mv_slide[0].setAttribute('style','opacity:1');
-            mvBtn[mvSlideNum].classList.add('mv_btn_active');
-        }else if(e.target == mvBtn[1]){
-            let mvSlideNum = 1;
-            let left = mvSlidesWidth*mvSlideNum;
-            mvSlides.setAttribute('style',`left:${-left}px`);
-            mv_slide[1].setAttribute('style','opacity:1');
-            mvBtn[mvSlideNum].classList.add('mv_btn_active');
-        }else if(e.target == mvBtn[2]){
-            let mvSlideNum = 2;
-            let left = mvSlidesWidth*mvSlideNum;
-            mvSlides.setAttribute('style',`left:${-left}px`);
-            mv_slide[2].setAttribute('style','opacity:1');
-            mvBtn[mvSlideNum].classList.add('mv_btn_active');
-        }
-    })
-})
-let mvSlideNum = 0;
-const autoSlide = setInterval(()=>{
-    mvBtn.forEach((e)=>{
-        e.classList.remove('mv_btn_active');    
-    })
-    if(mvSlideNum<=(mv_slide.length)-1){
-        let left = mvSlidesWidth*(mvSlideNum)
-        mvSlides.setAttribute('style',`left:${-left}px`);
-        mv_slide[mvSlideNum].setAttribute('style','opacity:1');
-        mvBtn[mvSlideNum].classList.add('mv_btn_active');
+let $mvSlide = document.querySelectorAll('.mv_slide');
+let $mvBtn = document.querySelectorAll('.mv_btn');
+let $mvBtnWrap = document.querySelector('.mv_btn_wrap');
+let mvSlideNum = 1;
+const stopI = setInterval(()=>{
+    if(mvSlideNum <= $mvSlide.length - 1){
+        $mvSlide[mvSlideNum].style.opacity = '1';
         mvSlideNum++
     }else{
-        mvSlideNum = 0;
+        for(i = 1; i < $mvSlide.length;i++){
+            $mvSlide[i].style.opacity = '0';
+        }
+        mvSlideNum=1;
     }
 },2500);
-mvSlides.addEventListener('click',()=>{
-    clearInterval(autoSlide);
+
+console.log($mvBtn);
+$mvBtn.forEach((e)=>{
+    e.addEventListener('click',(c)=>{
+        clearInterval(stopI);
+        console.log(c.target.classList);
+        let indexNum = Array.from($mvBtn).indexOf(c.target);
+        for(i = 0; i < $mvSlide.length;i++){
+                $mvSlide[i].style.opacity = '0';
+                $mvBtn[i].classList.remove('mv_btn_active');
+        }
+        c.target.classList.add('mv_btn_active');
+        $mvSlide[indexNum].style.opacity='1';
+    })
 })
-autoSlide();     
-
-
