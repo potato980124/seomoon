f2fca0550511fd4f0ea78141748e5c1b7f23e384
potato990124 <potato980124@gmail.com>
2022-12-16 17:47:39 +0900

    포트 수정

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,4 @@
 let app = require('../app');
-let port = process.env.PORT || 5000;
+let port = 3000;
 
-app.listen(port, () => {
-  console.log(`${port}로 express 실행`);
-  console.log(`http://localhost:${port}`);
-})
+app.listen(process.env.PORT || port;)
